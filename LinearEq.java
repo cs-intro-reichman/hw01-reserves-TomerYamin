a9c86a092ae50384a4d59dbbce868f09bd0649e0
Tomer Yamin <tomery@vintage-ip.com>
2024-01-19 12:10:24 +0200

    HW1

--- LinearEq.java
+++ LinearEq.java
@@ -10,10 +10,8 @@ public class LinearEq {
 	double b = Double.parseDouble(args[1]);
 	double c = Double.parseDouble(args[2]);
 
-	double x = ((c-b)/a);
-
-	System.out.println(a + " * " + " x " + " + " + b + " = " + c);
-	System.out.println ("x "  +  " = " + x); 
+	System.out.println(a + " * x + " + b + " = " + c);
+	System.out.println ("x = "  +  ((c-b)/a));
 
 
 	}
